75f5688e4b20166dc1863978db01a81a46dfb6f9
nazar <nazar.yurynets@gmail.com>
2021-11-12 15:03:51 +0200

    js

---
+++ lesson1/hm/main.js
@@ -0,0 +1,145 @@
+//- Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//    Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+// let hello = "hello";
+// let owu = "owu";
+// let com = "com";
+// let ua = "ua";
+// let num1 = 1;
+// let num10 = 10;
+// let num999 = 999;
+// let num123 = 123;
+// let num314 = 314;
+// let num27 = 27;
+// let num16 = 16;
+// let bool1 = true;
+// let bool2 = false;
+
+// console.log(hello)
+// console.log(owu)
+// console.log(com)
+// console.log(ua)
+// console.log(num1)
+// console.log(num10)
+// console.log(num999)
+// console.log(num123)
+// console.log(num314)
+// console.log(num27)
+// console.log(num16)
+// console.log(bool1)
+// console.log(bool2)
+//
+// alert(hello)
+// // alert(owu)
+// // alert(com)
+// // alert(ua)
+// // alert(num1)
+// // alert(num10)
+// // alert(num999)
+// // alert(num123)
+// // alert(num314)
+// // alert(num27)
+// // alert(num16)
+// // alert(bool1)
+// // alert(bool2)
+//
+//
+// document.write(hello + "<br/>");
+// document.write(owu + "<br/>");
+// document.write(com + "<br/>");
+// document.write(ua + "<br/>");
+// document.write(num1 + "<br/>");
+// document.write(num10 + "<br/>");
+// document.write(num999 + "<br/>");
+// document.write(num123 + "<br/>");
+// document.write(num314 + "<br/>");
+// document.write(num27 + "<br/>");
+// document.write(num16 + "<br/>");
+// document.write(bool1 + "<br/>");
+// document.write(bool2 + "<br/>");
+
+
+//- Переприсвоїти кожній змінній з завдання значення на довільне.
+ //   Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+ //owu = "hello";
+ //com = "ol";
+// ua = "ra";
+ //num1 = 2;
+// num10 = 20;
+// num999 = 888;
+// bool1 = false
+
+//console.log(owu)
+//console.log(com)
+///console.log(ua)
+//console.log(num1)
+//console.log(num10)
+//console.log(num999)
+//console.log(bool1)
+
+//alert(owu)
+//alert(com)
+//alert(ua)
+//alert(num1)
+//alert(num999)
+//alert(num10)
+//alert(bool1)
+
+
+
+//document.write(owu  + "<br/>")
+//document.write(com  + "<br/>")
+//document.write(ua  + "<br/>")
+//document.write(num1  + "<br/>")
+//document.write(num999  + "<br/>")
+//document.write(num10  + "<br/>")
+//document.write(bool1  + "<br/>")
+
+//(3) + (4) - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+// За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+// Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+//
+// let firstName = "Nazar";
+// let middleName = "Tarasovich";
+ //let lastName = "Yurinets";
+ //let age = 22;
+
+//let person = (`${firstName} ${middleName} ${lastName}`);
+// prompt(`Вітаю,${person}. Тобі ${age} років.`);
+ //prompt(firstName)
+// prompt(middleName)
+// prompt(age)
+
+ //(5) - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//let a = 11;
+// let b = "22";
+ //let d = true;
+ //let c = "top"
+ //console.log(typeof a)
+ //console.log(typeof b)
+// console.log(typeof d)
+ //console.log(typeof c)
+
+//(6) - Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+// // В однакових виразах не використовувати однакові оператори!!!
+
+//5 < 6 -> true
+//5 > 6 -> false
+//5 !== 6 -> false
+//5 == 6 -> false
+//10 === 10 -> true
+//10 == 10 -> true
+//10 > 10 -> false
+//10 < 10 -> false
+//10 ? 10 -> false
+//123 === '123' -> false
+//123 == '123' -> true
+
+ //- Подумайте Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
+    let str = "20";
+let a = 5;
+document.write(str + a + "<br/>"); - 205
+document.write(str - a + "<br/>");15
+document.write(str * "2" + "<br/>"); 40
+document.write(str / 2 + "<br/>"); 10
